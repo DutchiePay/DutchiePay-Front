79f8c73f4ce4bd889550e23d6862cc4fc01853e0
hyunjihub <syub98774@naver.com>
2024-08-21 23:40:55 +0900

    [Init] 프로젝트 동적 라우팅 추가

--- dutchiepay/src/app/_components/Header.jsx
+++ dutchiepay/src/app/_components/Header.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export default function Header() {
-  return <header className="h-12">헤더</header>;
+  return <header className="h-12 text-blue--500">헤더</header>;
 }
