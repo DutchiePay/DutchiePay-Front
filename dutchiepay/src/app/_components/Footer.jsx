5726dacfcb2b74038ca6fee85f0a9809dea32225
ParkYongHo1 <qkaxhf8823@naver.com>
2024-08-23 12:21:36 +0900

    [rename, design] roots폴더 routes폴더로 이름 변경, 헤더구현, 푸터 구현

--- dutchiepay/src/app/_components/Footer.jsx
+++ dutchiepay/src/app/_components/Footer.jsx
@@ -1,5 +1,30 @@
-import Image from 'next/image';
+import Image from "next/image";
+import logo from "../../../public/image/logo.jpg";
+import github from "../../../public/image/github.svg";
+import notion from "../../../public/image/notion.svg";
 
 export default function Header() {
-  return <header className="h-12">푸터</header>;
+  return (
+    <div className="w-[1020px] h-[60px] pr-[40px] pl-[40px] m-0 m-auto">
+      <div className="flex items-center relative w-full">
+        <Image
+          className="w-[90px] h-[54px] mr-[65px] "
+          src={logo}
+          alt="logo"
+          width={90}
+          height={54}
+        ></Image>
+        <p>copyrightⓒ</p>
+        <div className="flex justify-end w-full">
+          <Image
+            className="mr-[24px]"
+            src={github}
+            width={24}
+            height={24}
+          ></Image>
+          <Image src={notion} width={24} height={24}></Image>
+        </div>
+      </div>
+    </div>
+  );
 }
