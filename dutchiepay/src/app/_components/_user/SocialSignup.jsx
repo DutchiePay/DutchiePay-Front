62b94d5cd44444c38471652134073e533c211dc8
hyunjihub <syub98774@naver.com>
2024-10-02 12:08:29 +0900

    [fix] 소셜 회원가입 요청 axios에서 Link로 변경

--- dutchiepay/src/app/_components/_user/SocialSignup.jsx
+++ dutchiepay/src/app/_components/_user/SocialSignup.jsx
@@ -4,44 +4,31 @@ import '@/styles/globals.css';
 import '@/styles/user.css';
 
 import Image from 'next/image';
-import axios from 'axios';
 import kakao from '../../../../public/image/kakao.png';
 import naver from '../../../../public/image/naver.png';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import Cookies from 'universal-cookie';
 
 export default function SocialSignup() {
   const router = useRouter();
-
-  const handleSocial = async (type) => {
-    try {
-      const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/oauth/signup?type=${type}`
-      );
-      console.log(`${type} 회원가입 성공 : ${response.data}`);
-      router.push('/');
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const cookie = new Cookies();
 
   return (
     <div className="flex gap-[20px] h-[70px]">
-      <button
+      <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/oauth/signup?type=naver`}
         className="user-signup__button bg-[#00c73c] text-white"
-        type="button"
-        onClick={() => handleSocial('naver')}
       >
         <Image src={naver} width={40} height={40} alt="naver" />
         <p>네이버로 시작하기</p>
-      </button>
-      <button
+      </Link>
+      <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/oauth/signup?type=kakao`}
         className="user-signup__button bg-[#FBDB44]"
-        type="button"
-        onClick={() => handleSocial('kakao')}
       >
         <Image src={kakao} width={40} height={40} alt="kakao" />
         <span>카카오로 시작하기</span>
-      </button>
+      </Link>
+      <button onClick={()=>console.log(cookie.get('token'))}>test</button>
     </div>
   );
 }
