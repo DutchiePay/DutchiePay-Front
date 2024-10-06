8f542ff9d93425da48884e50ed710fd2797a9aee
ParkYongHo1 <qkaxhf8823@naver.com>
2024-10-06 23:58:38 +0900

    [refactor] 위치정보 중복되지 않도록 표시 방법 변경

--- dutchiepay/src/app/_components/_user/GetLocation.jsx
+++ dutchiepay/src/app/_components/_user/GetLocation.jsx
@@ -20,7 +20,23 @@ export default function getLocation() {
               }
             );
 
-            resolve(response.data.results[0].region.area2.name);
+            const area1 = response.data.results[0].region.area1.name;
+            const area2 = response.data.results[0].region.area2.name;
+
+            // 걉치는 구 이름 리스트
+            const districts = ['중구', '동구', '서구', '남구', '북구'];
+
+            let location;
+            if (districts.includes(area1)) {
+              // area1이 구 이름일 경우
+              location = `${area1} ${area2}`;
+            } else {
+              // area2가 띄어쓰기 있을 경우 앞부분만 보이게 처리
+              const area2Parts = area2.split(' ');
+              location = `${area2Parts[0]}`; // 첫 번째 부분만 사용
+            }
+
+            resolve(location);
           } catch (error) {
             resolve('위치 정보를 불러오는 도중 오류가 발생했습니다.');
           }
@@ -30,7 +46,7 @@ export default function getLocation() {
             '위치정보를 불러올 수 없어 기본 설정 지역으로 가입이 진행됩니다.'
           );
 
-          resolve('서울시 중구');
+          resolve('서울특별시 중구');
         }
       );
     } else {
