0521b38872745ae2aecae30d18f31254f0715ceb
hyunjihub <syub98774@naver.com>
2024-11-01 21:06:06 +0900

    [fix] 같은 브라우저 탭 내에서 session 데이터 공유되지 않는 오류 해결

--- dutchiepay/src/app/_components/_layout/Header.jsx
+++ dutchiepay/src/app/_components/_layout/Header.jsx
@@ -2,6 +2,7 @@
 
 import '@/styles/header.css';
 
+import { useCallback, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { usePathname, useRouter } from 'next/navigation';
 
@@ -15,61 +16,102 @@ import axios from 'axios';
 import chat from '/public/image/chat.svg';
 import { login } from '@/redux/slice/loginSlice';
 import logo from '/public/image/logo.jpg';
+import { logout } from '@/redux/slice/loginSlice';
 import profile from '/public/image/profile.jpg';
 import { setAddresses } from '@/redux/slice/addressSlice';
-import { useEffect } from 'react';
 
 export default function Header() {
   const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
   const user = useSelector((state) => state.login.user);
   const addresses = useSelector((state) => state.address.addresses);
+  const access = useSelector((state) => state.login.access);
   const dispatch = useDispatch();
   const router = useRouter();
   const pathname = usePathname();
 
-  useEffect(() => {
+  const handleRelogin = useCallback(async () => {
     const cookies = new Cookies();
     const refresh = cookies.get('refresh');
-    const handleRelogin = async () => {
-      try {
-        const response = await axios.post(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/users/relogin`,
-          { refresh: refresh }
-        );
 
-        const userInfo = {
-          isLoggedIn: true,
-          loginType: response.data.type || 'email',
-          user: {
-            userId: response.data.userId,
-            nickname: response.data.nickname,
-            profileImage: response.data.profileImg,
-            location: response.data.location,
-            isCertified: response.data.isCertified,
-          },
-          access: response.data.access,
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/users/relogin`,
+        { refresh: refresh }
+      );
+
+      const userInfo = {
+        isLoggedIn: true,
+        loginType: response.data.type || 'email',
+        user: {
+          userId: response.data.userId,
+          nickname: response.data.nickname,
+          profileImage: response.data.profileImg,
+          location: response.data.location,
+          isCertified: response.data.isCertified,
+        },
+        access: response.data.access,
+      };
+      localStorage.setItem('loginType', userInfo.loginType);
+      dispatch(
+        login({
+          user: userInfo.user,
+          access: userInfo.access,
+        })
+      );
+    } catch (error) {
+      console.log(error);
+      if (
+        error.response.data.message === '리프레시 토큰이 유효하지 않습니다.'
+      ) {
+        alert('자동로그인이 만료되었습니다. 다시 로그인해 주세요.');
+        cookies.remove('refresh', { path: '/' });
+      }
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (localStorage.getItem('dutchie-rememberMe') && !access) {
+      handleRelogin();
+    }
+  }, [access, handleRelogin]);
+
+  useEffect(() => {
+    if (!access && !localStorage.getItem('dutchie-rememberMe')) {
+      localStorage.setItem('storage-change', '1');
+      localStorage.removeItem('storage-change');
+    }
+  }, [access]);
+
+  useEffect(() => {
+    const handleStorageEvent = (e) => {
+      if (!e.newValue) return;
+
+      if (e.key === 'storage-change' && access) {
+        const sharedData = {
+          user,
+          access: access,
         };
-        localStorage.setItem('loginType', userInfo.loginType);
+        localStorage.setItem('sharedUserInfo', JSON.stringify(sharedData));
+        localStorage.removeItem('sharedUserInfo');
+      } else if (e.key === 'sharedUserInfo' && !access) {
+        if (e.oldValue) return;
+        const data = JSON.parse(e.newValue);
         dispatch(
           login({
-            user: userInfo.user,
-            access: userInfo.access,
+            user: data.user,
+            access: data.access,
           })
         );
-      } catch (error) {
-        if (
-          error.response.data.message === '리프레시 토큰이 유효하지 않습니다.'
-        ) {
-          alert('자동로그인이 만료되었습니다. 다시 로그인해 주세요.');
-          cookies.remove('refresh', { path: '/' });
-        }
+      } else if (e.key === 'logout-event' && access) {
+        dispatch(logout());
       }
     };
 
-    if (refresh && !isLoggedIn) {
-      handleRelogin();
-    }
-  }, [dispatch, isLoggedIn]);
+    window.addEventListener('storage', handleStorageEvent);
+    return () => {
+      window.removeEventListener('storage', handleStorageEvent);
+    };
+  }, [dispatch, access, user, isLoggedIn]);
 
   useEffect(() => {
     if (pathname === '/' && !isLoggedIn && addresses) {
--- dutchiepay/src/app/hooks/useLogout.jsx
+++ dutchiepay/src/app/hooks/useLogout.jsx
@@ -1,14 +1,14 @@
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+import { logout } from '@/redux/slice/loginSlice';
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import axios from 'axios';
 import { useRouter } from 'next/navigation';
-import { logout } from '@/redux/slice/loginSlice';
-import Cookies from 'universal-cookie';
 
 const useLogout = (accessToken) => {
   const dispatch = useDispatch();
   const router = useRouter();
-  const cookies = new Cookies();
+
   const handleLogout = useCallback(async () => {
     try {
       await axios.post(
@@ -22,11 +22,15 @@ const useLogout = (accessToken) => {
       );
 
       dispatch(logout());
+      const cookies = new Cookies();
       cookies.remove('refresh', { path: '/' });
       sessionStorage.removeItem('user');
+      localStorage.setItem('logout-event', '1');
+      localStorage.removeItem('logout-event');
+      localStorage.removeItem('dutchie-rememberMe');
       router.push('/');
     } catch (error) {
-      console.error('로그아웃 중 오류 발생:', error);
+      alert('오류가 발생했습니다. 다시 시도해주세요.');
     }
   }, [dispatch, accessToken, router]);
 
