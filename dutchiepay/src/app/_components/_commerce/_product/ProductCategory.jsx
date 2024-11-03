32348de665c1e6362e76f059341c0cf221fad1ce
hyunjihub <syub98774@naver.com>
2024-11-03 17:26:13 +0900

    [fix] 카테고리가 렌더링되지 않는 오류 해결

--- dutchiepay/src/app/_components/_commerce/_product/ProductCategory.jsx
+++ dutchiepay/src/app/_components/_commerce/_product/ProductCategory.jsx
@@ -17,21 +17,24 @@ export default function ProductCategory({ category, setCategory }) {
     <ul className="mt-[60px] flex justify-center gap-[32px] mx-auto my-0">
       {Object.entries(CATEGORIES).map(([key, value]) => {
         if (key !== '전체') {
-          <li
-            key={key}
-            className={`commerce-categories__item hover:text-blue--500 ${category === key ? 'text-blue--500' : ''}`}
-            onClick={(e) => handleCategory(e, key)}
-          >
-            <Image
-              className="w-[50px] h-[50px]"
-              src={CATEGORY_IMAGES[key]}
-              alt={value}
-              width={70}
-              height={70}
-            />
-            {key}
-          </li>;
+          return (
+            <li
+              key={key}
+              className={`commerce-categories__item hover:text-blue--500 ${category === key ? 'text-blue--500' : ''}`}
+              onClick={(e) => handleCategory(e)}
+            >
+              <Image
+                className="w-[50px] h-[50px]"
+                src={CATEGORY_IMAGES[key]}
+                alt={value}
+                width={70}
+                height={70}
+              />
+              {key}
+            </li>
+          );
         }
+        return null;
       })}
     </ul>
   );
