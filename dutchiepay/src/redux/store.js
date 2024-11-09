72a10882f9e256d5f8a06449eb81423cb2a81ae9
hyunjihub <syub98774@naver.com>
2024-11-09 21:25:37 +0900

    [fix] SSR에서 redux-persist가 제대로 동작하지 않는 오류 해결

--- dutchiepay/src/redux/store.js
+++ dutchiepay/src/redux/store.js
@@ -3,8 +3,29 @@ import { persistReducer, persistStore } from 'redux-persist';
 import addressReducer from './slice/addressSlice';
 import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
 import loginReducer from '../redux/slice/loginSlice';
-import sessionStorage from 'redux-persist/lib/storage/session';
+
+// 서버 사이드에서 오류가 발생하지 않도록 하는 더미 스토리지 생성
+const createNoopStorage = () => {
+  return {
+    getItem(_key) {
+      return Promise.resolve(null);
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key) {
+      return Promise.resolve();
+    },
+  };
+};
+
+// 클라이언트와 서버를 구분하여 스토리지 설정
+const storage =
+  typeof window !== 'undefined'
+    ? createWebStorage('session')
+    : createNoopStorage();
 
 const rootReducer = combineReducers({
   address: addressReducer,
@@ -13,7 +34,7 @@ const rootReducer = combineReducers({
 
 const persistConfig = {
   key: 'root',
-  storage: sessionStorage,
+  storage: storage,
   timeout: 1000,
 };
 
@@ -23,7 +44,7 @@ const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false, // serializableCheck를 사용하지 않도록 함
+      serializableCheck: false, // serializableCheck를 사용하지 않도록 설정
     }),
 });
 
