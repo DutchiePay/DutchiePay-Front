7fd25737b6488351263d860565de37e8c4579ebe
hyunjihub <syub98774@naver.com>
2024-11-10 14:06:26 +0900

    [fix] 오류메시지 변수 오류 수정

--- dutchiepay/src/app/hooks/useFetchOrderProduct.jsx
+++ dutchiepay/src/app/hooks/useFetchOrderProduct.jsx
@@ -20,12 +20,14 @@ export default function useFetchOrderProduct({ buyId, setOrderInfo }) {
         );
         setOrderInfo(response.data);
       } catch (error) {
-        if (error.data.message === '마감된 상품입니다.') {
+        if (error.response.data.message === '마감된 상품입니다.') {
           alert(
             '상품 구매가 마감되어 주문하실 수 없습니다. 메인으로 이동합니다.'
           );
           router.push('/');
-        } else if (error.data.message === '상품 정보를 찾을 수 없습니다.') {
+        } else if (
+          error.response.data.message === '상품 정보를 찾을 수 없습니다.'
+        ) {
           alert('잘못된 접근입니다. 메인으로 이동합니다.');
           router.push('/');
         } else alert('오류가 발생했습니다. 다시 시도해주세요.');
