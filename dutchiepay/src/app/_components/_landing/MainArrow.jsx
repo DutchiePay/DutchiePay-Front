223a7e303cab55d2f2fa74e3323e5190f9cf6eb9
hyunjihub <syub98774@naver.com>
2024-12-02 17:44:07 +0900

    [fix] MainArrow ref 제거

--- dutchiepay/src/app/_components/_landing/MainArrow.jsx
+++ dutchiepay/src/app/_components/_landing/MainArrow.jsx
@@ -1,11 +1,9 @@
 import Image from 'next/image';
-import { forwardRef } from 'react';
 import prevNnext from '/public/image/prevNnext.svg';
 
-const MainArrow = forwardRef(({ onClick, direction }, ref) => {
+const MainArrow = ({ onClick, direction }) => {
   return (
     <button
-      ref={ref}
       className="w-[44px] h-[44px] bg-black/30 rounded-full flex justify-center items-center"
       onClick={onClick}
     >
@@ -18,6 +16,6 @@ const MainArrow = forwardRef(({ onClick, direction }, ref) => {
       />
     </button>
   );
-});
+};
 
 export default MainArrow;
