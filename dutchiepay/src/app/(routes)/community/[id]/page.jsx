fe0f29b27f291e03daa1a7d7847c23e62ba6bdb8
ParkYongHo1 <qkaxhf8823@naver.com>
2024-12-12 21:17:19 +0900

    [feat] 자유게시판 상세페이지 추천 게시글 구현

--- dutchiepay/src/app/(routes)/community/[id]/page.jsx
+++ dutchiepay/src/app/(routes)/community/[id]/page.jsx
@@ -4,10 +4,9 @@ import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 
 import PostContent from '@/app/_components/_community/_common/PostContent';
-import Post_Hot from '@/app/_components/_community/_free/Post_Hot';
-import Post_Similar from '@/app/_components/_community/_free/Post_Similar';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
+import PostRecommend from '@/app/_components/_community/_free/PostRecommend';
 
 export default function CommunityDetail() {
   const { id } = useParams();
@@ -40,38 +39,17 @@ export default function CommunityDetail() {
         }
       }
     };
-
     fetchCommunityDetail();
   }, [id, access, router]);
 
   return (
     <section className="min-h-[750px] w-[1020px]">
-      <div className="flex justify-between">
-        {post && <PostContent menu={'community'} post={post} postId={id} />}
-
-        <section className="w-[290px] h-[750px] sticky top-[150px] pl-[24px] py-[40px] flex flex-col gap-[40px]">
-          <article>
-            <h2 className="text-2xl font-bold">유사한 게시글</h2>
-            <div className="flex flex-col gap-[12px] mt-[8px]">
-              <Post_Similar />
-              <Post_Similar />
-              <Post_Similar />
-              <Post_Similar />
-              <Post_Similar />
-            </div>
-          </article>
-          <article>
-            <h2 className="text-2xl font-bold">주간 HOT🔥 게시글</h2>
-            <div className="flex flex-col gap-[12px] mt-[8px]">
-              <Post_Hot index={1} />
-              <Post_Hot index={2} />
-              <Post_Hot index={3} />
-              <Post_Hot index={4} />
-              <Post_Hot index={5} />
-            </div>
-          </article>
-        </section>
-      </div>
+      {post && (
+        <div className="flex justify-between">
+          <PostContent menu={'community'} post={post} postId={id} />
+          <PostRecommend post={post} />
+        </div>
+      )}
     </section>
   );
 }
